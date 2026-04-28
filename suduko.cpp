db559fcd5dc25eb480af66ec4dafdf592d9c7bcc
amansharma200621-svg <amansharma200621@gmail.com>
2026-04-28 19:29:40 +0530

    Create suduko.cpp

---
+++ suduko.cpp
@@ -0,0 +1,29 @@
+#include "SudokuSolver.h"
+
+bool SudokuSolver::isSafe(const std::vector<std::vector<int>>& board, int row, int col, int num) {
+    for (int i = 0; i < 9; i++) {
+        if (board[row][i] == num || board[i][col] == num || 
+            board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == num) {
+            return false;
+        }
+    }
+    return true;
+}
+
+bool SudokuSolver::solve(std::vector<std::vector<int>>& board) {
+    for (int row = 0; row < 9; row++) {
+        for (int col = 0; col < 9; col++) {
+            if (board[row][col] == 0) {
+                for (int num = 1; num <= 9; num++) {
+                    if (isSafe(board, row, col, num)) {
+                        board[row][col] = num;
+                        if (solve(board)) return true;
+                        board[row][col] = 0; // Backtrack
+                    }
+                }
+                return false;
+            }
+        }
+    }
+    return true;
+}
